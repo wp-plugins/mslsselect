f5082d8627f53b297d0309f44f470e173d22135f
realloc <realloc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 13:29:36 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/mslsselect/trunk@1142151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MslsSelect.php
+++ MslsSelect.php
@@ -4,7 +4,7 @@
 Plugin Name: MslsSelect
 Plugin URI: https://github.com/lloc/MslsSelect
 Description: Transforms the output of the Multisite Language Switcher to an HTML select
-Version: 0.1
+Version: 1.0
 Author: Dennis Ploetner
 Author URI: http://lloc.de/
 */
@@ -32,41 +32,76 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 class MslsSelect {
 
-	const VERSION = '0.1';
+	const VERSION = '1.0';
 
-	public function __construct() {
-		add_action( 'wp_enqueue_scripts',   array( $this, 'enqueue_scripts' ) );
-		add_filter( 'msls_output_get_tags', array( $this, 'get_tags' ) );
-		add_filter( 'msls_output_get',      array( $this, 'output_get' ), 10, 3 );
+	/**
+	 * Factory
+	 *
+	 * @return MslsSelect
+	 */
+	public static function init() {
+		$obj = new self();
+
+		if ( ! is_admin() ) {
+			add_action( 'wp_enqueue_scripts', array( $obj, 'enqueue_scripts' ) );
+			add_filter( 'msls_output_get_tags', array( $obj, 'get_tags' ) );
+			add_filter( 'msls_output_get', array( $obj, 'output_get' ), 10, 3 );
+		}
+
+		return $obj;
 	}
 
+	/**
+	 * Enqueue scripts action
+	 */
 	public function enqueue_scripts() {
 		wp_enqueue_script(
 			'mslsselect',
-			plugins_url( '/js/mslsselect.js' , __FILE__ ),
+			plugins_url( '/js/mslsselect.js', __FILE__ ),
 			array( 'jquery' ),
 			self::VERSION,
 			true
 		);
 	}
 
+	/**
+	 * Filter for the 'msls_output_get'-hook
+	 *
+	 * @param string $url
+	 * @param StdClass $link
+	 * @param bool $current
+	 *
+	 * @return string
+	 */
 	public function output_get( $url, $link, $current ) {
 		return sprintf(
-			'<option value="%s"%s>%s</option>',
+			'<option value="%s" %s>%s</option>',
 			$url,
 			( $current ? ' selected="selected"' : '' ),
 			$link->txt
 		);
 	}
 
+	/**
+	 * Filter for the 'msls_output_get_tags'-hook
+	 *
+	 * @param array $tags
+	 *
+	 * @return array
+	 */
 	public function get_tags( $tags ) {
-		return array(
+		$tags = array(
 			'before_item'   => '',
 			'after_item'    => '',
 			'before_output' => '<select class="msls_languages">',
 			'after_output'  => '</select>',
 		);
+
+		return $tags;
 	}
 
 }
-$mslsselect = new MslsSelect(); 
+
+add_action( 'plugins_loaded', function () {
+	MslsSelect::init();
+} );
